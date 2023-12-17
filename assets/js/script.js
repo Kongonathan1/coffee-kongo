9d9ed36cecb2007899cb1685e2d2b66d24248e3d
Kongonathan1 <143329639+Kongonathan1@users.noreply.github.com>
2023-12-17 12:23:07 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,29 @@ const swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
+
+const menu = document.querySelector('.menu-icon')
+const navbar = document.querySelector('.navbar') 
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('move')
+    navbar.classList.toggle('open-navbar')
+})
+
+window.onscroll = () => {
+    menu.classList.remove('move')
+    navbar.classList.remove('open-navbar')
+}
+
+const scrollReveal = new ScrollReveal({
+    origin: "top",
+    distance: '60px',
+    duration: "2500",
+    delay: "400",
+})
+
+scrollReveal.reveal(".nav")
+scrollReveal.reveal(".left-content", {origin: "left"})
+scrollReveal.reveal(".right-content", {origin: "bottom"})
+scrollReveal.reveal(".service-content, .product-card, .team-card, .book-data", {interval: 100})
